53b6de4fd8e133a211d57dca1f3f8b8594973d82
julian <juliandecastelli@gmail.com>
2022-08-03 12:28:01 -0300

    add mongoose and mongodb

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+app.use(cors());
+app.use(express.static("public"));
 
+const userSchema = new mongoose.Schema({
+  username: String,
+});
+const User = mongoose.model("User", userSchema);
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.post("/api/users/", (req, res) => {
+  const username = req.body.username;
+  console.log(username);
+  new User({
+    username: username,
+  });
+
+  res.json({ g: username });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});

0291d8f3e765cdfc15e6ad036ebc165002625c56
julian <juliandecastelli@gmail.com>
2022-08-03 21:18:20 -0300

    add get for users and logs

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.static("public"));
 
 const userSchema = new mongoose.Schema({
   username: String,
+  count: 0,
+  log: [{}],
 });
 const User = mongoose.model("User", userSchema);
 
@@ -33,9 +35,74 @@ app.post("/api/users/", (req, res) => {
   console.log(username);
   new User({
     username: username,
+  }).save((err, data) => {
+    if (err) console.error("error", err);
+    res.json({ username: data.username, _id: data._id });
   });
+});
+app.get("/api/users", (req, res) => {
+  User.find({ username: /.*/g })
+    .select("username")
+    .exec((err, data) => {
+      if (err) console.error("error", err);
+      console.log(data);
+      res.json(data);
+    });
+});
+app.post("/api/users/:id/exercises", (req, res) => {
+  const description = req.body.description;
+  const duration = req.body.duration;
+  console.log(req.body.date);
+  let date;
+  if (req.body.date == "" || req.body.date == undefined) {
+    date = new Date();
+  } else {
+    date = new Date(req.body.date);
+    date.setDate(date.getDate() + 1);
+  }
 
-  res.json({ g: username });
+  date = date.toDateString();
+  const id = req.params.id;
+  const exercise = {
+    description: description,
+    duration: Number(duration),
+    date: date,
+  };
+  User.findOneAndUpdate(
+    { _id: id },
+    { $push: { log: exercise }, $inc: { count: +1 } },
+    { new: true }
+  ).exec((err, data) => {
+    if (err) console.error("error", err);
+    res.json({
+      _id: data._id,
+      username: data.username,
+      date: date,
+      duration: duration,
+      description: description,
+    });
+  });
+});
+app.get("/api/users/:id/logs", (req, res) => {
+  const { from, to, limit } = req.query;
+  const id = req.params.id;
+
+  User.findById({ _id: id })
+    .select("-__v")
+    .exec((err, data) => {
+      if (err) console.error("error", err);
+      let logs = data.log;
+      let filteredLogs = logs.filter((log) => {
+        const logDate = new Date(log.date).getTime();
+        const toMS = new Date(to).getTime();
+        const fromMS = new Date(from).getTime();
+
+        console.log(log.date);
+        return logDate > fromMS && logDate < toMS;
+      });
+      console.log(filteredLogs);
+      res.json(data);
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
